458ed8826b8924fc1745a2e3b9077703b2a5503f
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-04-27 03:55:10 +0100

    contact component

--- src/Contact.js
+++ src/Contact.js
@@ -4,6 +4,12 @@ import images from './images/logo.png'
 import location from './images/location.png'
 import phone from './images/phone.png'
 import envelope from './images/envelope.png'
+import IG from './images/IG-1.jpg'
+import IG1 from './images/IG-2.jpg'
+import IG2 from './images/IG-3.jpg'
+import IG3 from './images/IG-4.jpg'
+import IG4 from './images/IG-5.jpg'
+import IG5 from './images/IG-6.jpg'
 
 
 function Contact() {
@@ -33,22 +39,25 @@ function Contact() {
           <div className='widget'>
               <h2>TWITTER WIDGET</h2>
               <div className='socials'>
-                  <h6>lorem18</h6>
-                  <div>
+                  <h6>intelligence-sharing also paved the way for Ukraine to shoot down a Russian transport plane carrying hundreds of troops in the early days of the war, the officials say, helping repel a Russian assault on a key airport near Kyiv.
+                    The details about the air defenses and the transport plane</h6>
+                  <div className='feeds'>
                       <img src='' alt='' />
                       <h6>5 min ago</h6>
                   </div>
               </div>
               <div className='socials'>
-                  <h6>lorem18</h6>
-                  <div>
+                  <h6>intelligence-sharing also paved the way for Ukraine to shoot down a Russian transport plane carrying hundreds of troops in the early days of the war, the officials say, helping repel a Russian assault on a key airport near Kyiv.
+                    The details about the air defenses and the transport plane</h6>
+                  <div className='feeds'>
                       <img src='' alt='' />
                       <h6>5 min ago</h6>
                   </div>
               </div>
           </div>
           <div className='usefulLinks'>
-              <ul>
+              <h2>USEFUL LINKS</h2>
+              <ul className='links-ul'>
                   <li>ABOUT US</li>
                   <li>FEATURES</li>
                   <li>PRIVATE POLICY</li>
@@ -57,13 +66,17 @@ function Contact() {
                   <li>Photo Gallery</li>
             </ul>
           </div>
-          <div className='instagram'>
-                <img src='' alt=''/>
-                <img src='' alt=''/>
-                <img src='' alt=''/>
-                <img src='' alt=''/>
-                <img src='' alt=''/>
-                <img src='' alt=''/>
+
+          <div className='IG'>
+              <h3>INSTAGRAM</h3>
+             <div className='instagram'>
+                <img src={IG} alt=''/>
+                <img src={IG1} alt=''/>
+                <img src={IG2} alt=''/>
+                <img src={IG3} alt=''/>
+                <img src={IG4} alt=''/>
+                <img src={IG5} alt=''/>
+          </div>
           </div>
         </div>
   )

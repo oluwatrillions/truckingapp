49c6f9ee96f14e93354be05c93f66f064f7a84a7
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-04-29 00:19:54 +0100

    ...

--- src/Offers.js
+++ src/Offers.js
@@ -6,66 +6,73 @@ import contract from './images/contract.png'
 import support from './images/support.png'
 import destination from './images/destination.png'
 import tracking from './images/tracking.png'
+import { Container } from '@material-ui/core';
 
 function Offers() {
-    return <div className='offer'>
-        <div className='offer-story'>
-            <h1>OUR <span>AWESOME OFFERS</span></h1>
-            <h6>Search our databases for the Top Providers in key segments of <br /><span>
-            the supply chain industry.</span></h6>
-        </div>
-        <div className='offers'>
-            <div className='offer-divs'>
-                <img src={warehouse} alt='' />
-                <div className='firstoffer'>
-                    <h3>Largest <span>Warehouse</span></h3>
-                    <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
-                        text commonly used to demonstrate the</h6>
-                </div>                
-            </div>
-        <div className='offer-divs'>
-                <img src={logistic} alt='' />
-                <div className='firstoffer'>
-                    <h3>Logistic <span>Services</span></h3>
-                    <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
-                        text commonly used to demonstrate the</h6>
-                </div>                
-            </div>
-        <div className='offer-divs'>
-                <img src={contract} alt='' />
-                <div className='firstoffer'>
-                    <h3>Contract <span>Logistic</span></h3>
-                    <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
-                        text commonly used to demonstrate the</h6>
-                </div>                
-            </div>
-        <div className='offer-divs'>
-                <img src={support} alt='' />
-                <div className='firstoffer'>
-                    <h3>Customer <span>Support</span></h3>
-                    <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
-                        text commonly used to demonstrate the</h6>
-                </div>                
-            </div>
-        <div className='offer-divs'>
-                <img src={destination} alt='' />
-                <div className='firstoffer'>
-                    <h3>Largest <span>Destinations</span></h3>
-                    <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
-                        text commonly used to demonstrate the</h6>
-                </div>                
-            </div>
-        <div className='offer-divs'>
-                <img src={tracking} alt='' />
-                <div className='firstoffer'>
-                    <h3>Goods <span>Tracking</span></h3>
-                    <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
-                        text commonly used to demonstrate the</h6>
-                </div>                
-            </div>
-        </div>
-        <button>VIEW <span>ALL OFFER</span></button>
-  </div>;
+    return <Container className='offer' maxWidth='xl'>
+        <div className='offer'>
+            <Container maxWidth='md'>
+                <div className='offer-story'>
+                    <h1>OUR <span>AWESOME OFFERS</span></h1>
+                    <h6>Search our databases for the Top Providers in key segments of <br /><span>
+                    the supply chain industry.</span></h6>
+                </div>
+            </Container>
+            <Container maxWidth='xl' className='offers'>
+                <div className='offers'>
+                    <div className='offer-divs'>
+                        <img src={warehouse} alt='' />
+                        <div className='firstoffer'>
+                            <h3>Largest <span>Warehouse</span></h3>
+                            <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
+                                text commonly used to demonstrate the</h6>
+                        </div>                
+                    </div>
+                    <div className='offer-divs'>
+                            <img src={logistic} alt='' />
+                            <div className='firstoffer'>
+                                <h3>Logistic <span>Services</span></h3>
+                                <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
+                                    text commonly used to demonstrate the</h6>
+                            </div>                
+                        </div>
+                    <div className='offer-divs'>
+                            <img src={contract} alt='' />
+                            <div className='firstoffer'>
+                                <h3>Contract <span>Logistic</span></h3>
+                                <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
+                                    text commonly used to demonstrate the</h6>
+                            </div>                
+                        </div>
+                    <div className='offer-divs'>
+                            <img src={support} alt='' />
+                            <div className='firstoffer'>
+                                <h3>Customer <span>Support</span></h3>
+                                <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
+                                    text commonly used to demonstrate the</h6>
+                            </div>                
+                        </div>
+                    <div className='offer-divs'>
+                            <img src={destination} alt='' />
+                            <div className='firstoffer'>
+                                <h3>Largest <span>Destinations</span></h3>
+                                <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
+                                    text commonly used to demonstrate the</h6>
+                            </div>                
+                        </div>
+                    <div className='offer-divs'>
+                            <img src={tracking} alt='' />
+                            <div className='firstoffer'>
+                                <h3>Goods <span>Tracking</span></h3>
+                                <h6>In publishing and graphic design, Lorem ipsum is a placeholder <br />
+                                    text commonly used to demonstrate the</h6>
+                            </div>                
+                    </div>
+                </div>
+            </Container>
+            <button>VIEW <span>ALL OFFER</span></button>
+        </div>;
+        </Container>
 }
 
 export default Offers;

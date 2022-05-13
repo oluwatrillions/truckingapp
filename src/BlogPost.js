8c4bfcf7e52e3259dd338a78ebdb46fea4e58bd6
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-05-13 18:04:27 +0100

    ..

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import image from './images/shipp.png'
 import './Blogpost.css'
-import { Container } from '@material-ui/core';
 
 function BlogPost({dataInfo}) {
     return <div>
       {dataInfo.map((datas) => {
         const {id, day, month, name, time, description} = datas
-        return <Container>
-        <div className='blogpost' key={id}>
+        return <div className='blogpost' key={id}>
             <div className='dataInfo'>
                 <img src={image} alt='' />
                 <h4>{day}</h4>
@@ -19,8 +17,7 @@ function BlogPost({dataInfo}) {
             <h5 className='blogDesc'>{description}</h5>
             <h4 className='blogMore'>READ MORE</h4>
           </div>
-          </div>
-          </Container>
+          </div>          
       })}
   </div>
 }
